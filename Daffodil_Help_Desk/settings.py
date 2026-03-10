8c724e47eab17ecb5ab9de6ace1f2da18d5eb4f9
Arafath Abir <arafathabir07@gmail.com>
2026-03-11 02:58:18 +0600

    added database

--- Daffodil_Help_Desk/settings.py
+++ Daffodil_Help_Desk/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Daffodil_Help_Desk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'helpdesk_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
